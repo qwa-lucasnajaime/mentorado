75f5b538f2996270ab5967e80c9caf0fc0ca7db7
Fernando Nissizaki <fernando.nissizaki@qwasolucoes.com.br>
2021-10-22 15:16:14 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/qwasolucoes/teste_avaliacao/implementacoes/TesteUm.java
@@ -0,0 +1,11 @@
+package br.com.qwasolucoes.teste_avaliacao.implementacoes;
+
+import br.com.qwasolucoes.teste_avaliacao.interfaces.ITesteUm;
+
+public class TesteUm implements ITesteUm {
+
+  public int soma(int numeroUm, int numeroDois) {
+    return 0;
+  }
+  
+}
---
+++ src/main/java/br/com/qwasolucoes/teste_avaliacao/interfaces/ITesteUm.java
@@ -0,0 +1,5 @@
+package br.com.qwasolucoes.teste_avaliacao.interfaces;
+
+public interface ITesteUm {
+  int soma(int numeroUm, int numeroDois);
+}
